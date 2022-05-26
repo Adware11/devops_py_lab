4028ed6d1a829e3487380dedfd8cbce2e0c4afb3
Adware11 <104993296+Adware11@users.noreply.github.com>
2022-05-26 09:16:55 +0300

    Update main_ver_1.py

--- py/main_ver_1.py
+++ py/main_ver_1.py
@@ -8,7 +8,15 @@ tmpl_slides_file="tmpl/slides_ver_1.md"
 out_md_file="out/slides_ver_1.md"
 out_pptx_file="out/slides_ver_1.md"
 
-pass # объединить файлы в один в директории out 
 
-m2o.convert_to_pptx(out_md_file, out_pptx_file)  # преобразовать в pptx 
+# объединить файлы в один в директории out 
+filenames = [tmpl_category_file, tmpl_slides_file]
+with open(out_md_file, "w") as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(infile.read())
+        outfile.write("\n")
 
+# преобразовать в pptx 
+m2o.convert_to_pptx(out_md_file, out_pptx_file) 
